971243c30027463aa9fe44d2c991b108006882d6
Phuong Nguyen <pnguyen@phuong-lin.(none)>
2012-05-11 21:45:41 -0400

    support multi-value indexing

--- src/main/java/com/hmsonline/cassandra/index/Configuration.java
+++ src/main/java/com/hmsonline/cassandra/index/Configuration.java
@@ -1,7 +1,8 @@
 package com.hmsonline.cassandra.index;
 
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -11,9 +12,10 @@ public class Configuration {
     public static final String KEYSPACE = "keyspace";
     public static final String COLUMN_FAMILY = "column_family";
     public static final String COLUMNS = "columns";
-    private static final String COLUMN_DELIM = ",";
-    
-    private Map<String, Map<String, Set<String>>> config = new HashMap<String, Map<String, Set<String>>>();
+    public static final String COLUMN_DELIM = ",";
+    public static final String FIELD_DELIM = ":";
+
+    private Map<String, Map<String, List<String>>> config = new HashMap<String, Map<String, List<String>>>();
 
     public void addIndex(String indexName, Map<String, String> indexProperties) {
         String keyspace = indexProperties.get(KEYSPACE);
@@ -24,30 +26,30 @@ public class Configuration {
             return;
         }
 
-        Set<String> columnSet = new LinkedHashSet<String>();
+        List<String> indexColumns = new ArrayList<String>();
         for (String column : columns.split(COLUMN_DELIM)) {
             if (StringUtils.isNotEmpty(column)) {
-                columnSet.add(column.trim());
+                indexColumns.add(column.trim());
             }
         }
 
         String key = generateKey(keyspace, columnFamily);
         if (!config.containsKey(key)) {
-            config.put(key, new HashMap<String, Set<String>>());
+            config.put(key, new HashMap<String, List<String>>());
         }
-        config.get(key).put(indexName, columnSet);
+        config.get(key).put(indexName, indexColumns);
     }
 
-    public Map<String, Set<String>> getIndexes(String keyspace, String columnFamily) {
+    public Map<String, List<String>> getIndexes(String keyspace, String columnFamily) {
         String key = generateKey(keyspace, columnFamily);
-        return config.containsKey(key) ? config.get(key) : new HashMap<String, Set<String>>();
+        return config.containsKey(key) ? config.get(key) : new HashMap<String, List<String>>();
     }
 
     public Set<String> getIndexNames(String keyspace, String columnFamily) {
         return getIndexes(keyspace, columnFamily).keySet();
     }
 
-    public Set<String> getIndexColumns(String keyspace, String columnFamily, String indexName) {
+    public List<String> getIndexColumns(String keyspace, String columnFamily, String indexName) {
         return getIndexes(keyspace, columnFamily).get(indexName);
     }
 
--- src/main/java/com/hmsonline/cassandra/index/util/IndexUtil.java
+++ src/main/java/com/hmsonline/cassandra/index/util/IndexUtil.java
@@ -2,126 +2,248 @@ package com.hmsonline.cassandra.index.util;
 
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import net.sf.json.JSON;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+import net.sf.json.JSONSerializer;
+
 import org.apache.cassandra.db.ColumnFamily;
 import org.apache.cassandra.db.IColumn;
-import org.apache.cassandra.db.IMutation;
-import org.apache.cassandra.db.RowMutation;
 import org.apache.cassandra.thrift.CassandraServer;
 import org.apache.cassandra.thrift.ColumnOrSuperColumn;
 import org.apache.cassandra.thrift.ColumnParent;
 import org.apache.cassandra.thrift.ConsistencyLevel;
 import org.apache.cassandra.thrift.SlicePredicate;
+import org.apache.cassandra.thrift.SliceRange;
 import org.apache.cassandra.utils.ByteBufferUtil;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import com.hmsonline.cassandra.index.LogEntry;
-import com.hmsonline.cassandra.index.LogEntry.Status;
+import com.hmsonline.cassandra.index.Configuration;
 
-/**
- * A helper class providing utility methods for handling indexing.
- * 
- * @author pnguyen
- */
 public class IndexUtil {
     public static final String INDEXING_KEYSPACE = "Indexing";
+    private static Logger logger = LoggerFactory.getLogger(IndexUtil.class);
 
-    public static ByteBuffer buildIndex(Set<String> indexColumns, String rowKey, Map<String, String> row)
+    public static List<ByteBuffer> buildIndexes(List<String> indexColumns, String rowKey, Map<String, Set<String>> row)
             throws Exception {
-        List<String> parts = new ArrayList<String>();
-        for (String columnName : indexColumns) {
-            parts.add(row.get(columnName));
-        }
-        parts.add(rowKey);
-        return CompositeUtil.compose(parts);
-    }
-
-    public static Map<String, String> getRowMutation(ColumnFamily columnFamily) throws Exception {
-        Map<String, String> mutation = new HashMap<String, String>();
-        for (IColumn column : columnFamily.getSortedColumns()) {
-            String value = column.isMarkedForDelete() ? null : ByteBufferUtil.string(column.value());
-            mutation.put(ByteBufferUtil.string(column.name()), value);
-        }
-        return mutation;
-    }
-
-    public static Map<String, String> getRowMutation(ColumnFamily columnFamily, Set<String> indexColumns)
-            throws Exception {
-        Map<String, String> mutation = new HashMap<String, String>();
-        for (IColumn column : columnFamily.getSortedColumns()) {
-            String columnName = ByteBufferUtil.string(column.name());
-            if (!indexColumns.contains(columnName)) {
-                continue;
+        // Calculate number of indexes
+        int numIndexes = 0;
+        for (String indexColumn : indexColumns) {
+            Set<String> values = row.get(indexColumn);
+            if (!values.isEmpty()) {
+                numIndexes = numIndexes == 0 ? values.size() : numIndexes * values.size();
             }
-            mutation.put(columnName, column.isMarkedForDelete() ? null : ByteBufferUtil.string(column.value()));
         }
-        return mutation;
+
+        if (numIndexes == 0) {
+            return new ArrayList<ByteBuffer>();
+        }
+
+        // Create empty indexes with the last part filled with rowkey
+        List<String[]> indexes = new ArrayList<String[]>();
+        for (int i = 0; i < numIndexes; i++) {
+            String[] parts = new String[indexColumns.size() + 1];
+            parts[parts.length - 1] = rowKey;
+            indexes.add(parts);
+        }
+
+        // Fill in indexes with component values
+        setIndexValues(indexes, indexColumns, row, 0);
+
+        // Build indexes
+        List<ByteBuffer> result = new ArrayList<ByteBuffer>();
+        for (String[] parts : indexes) {
+            result.add(CompositeUtil.compose(Arrays.asList(parts)));
+        }
+
+        return result;
     }
 
-    public static Map<String, String> getNewRow(Map<String, String> currentRow, ColumnFamily columnFamily,
-            Set<String> indexColumns) throws Exception {
-        Map<String, String> newRow = new HashMap<String, String>(currentRow);
-        newRow.putAll(getRowMutation(columnFamily, indexColumns));
-        return newRow;
+    private static void setIndexValues(List<String[]> indexes, List<String> indexColumns, Map<String, Set<String>> row,
+            int pos) {
+        if (pos >= indexColumns.size()) {
+            return;
+        }
+
+        Set<String> values = row.get(indexColumns.get(pos));
+        if (!values.isEmpty()) {
+            int count = 0, n = indexes.size() / values.size();
+            for (int i = 0; i < n; i++) {
+                for (String value : values) {
+                    indexes.get(count++)[pos] = value;
+                }
+            }
+        }
+
+        setIndexValues(indexes, indexColumns, row, pos + 1);
     }
 
-    public static boolean indexChanged(Set<String> indexColumns, ColumnFamily columnFamily) throws Exception {
+    public static boolean indexChanged(ColumnFamily columnFamily, Collection<String> indexColumns) throws Exception {
         for (ByteBuffer columnName : columnFamily.getColumnNames()) {
-            if (indexColumns.contains(ByteBufferUtil.string(columnName))) {
+            if (contains(indexColumns, ByteBufferUtil.string(columnName))) {
                 return true;
             }
         }
         return false;
     }
 
-    public static boolean isEmptyIndex(Set<String> indexColumns, Map<String, String> row) {
-        for (String column : indexColumns) {
-            if (row.get(column) != null) {
-                return false;
+    private static boolean contains(Collection<String> indexColumns, String columnName) {
+        for (String indexColumn : indexColumns) {
+            if (isMultiValueColumn(indexColumn)) {
+                if (columnName.startsWith(getColumnPrefix(indexColumn))) {
+                    return true;
+                }
+            } else {
+                if (columnName.equals(indexColumn)) {
+                    return true;
+                }
             }
         }
-        return true;
+
+        return false;
+    }
+
+    public static Map<String, Set<String>> getIndexValues(Map<String, String> row, Collection<String> indexColumns) {
+        Map<String, Set<String>> result = new HashMap<String, Set<String>>();
+
+        for (String indexColumn : indexColumns) {
+            Set<String> values = new HashSet<String>();
+
+            if (isMultiValueColumn(indexColumn)) {
+                String[] path = indexColumn.split(Configuration.FIELD_DELIM);
+                for (String columnName : row.keySet()) {
+                    if (columnName.startsWith(path[0])) {
+                        values.addAll(getJsonValues(row.get(columnName), path));
+                    }
+                }
+            } else {
+                if (StringUtils.isNotEmpty(row.get(indexColumn))) {
+                    values.add(row.get(indexColumn));
+                }
+            }
+
+            result.put(indexColumn, values);
+        }
+
+        return result;
+    }
+
+    private static List<String> getJsonValues(String jsonString, String[] path) {
+        JSON json = null;
+        try {
+            json = JSONSerializer.toJSON(jsonString);
+        } catch (Exception ex) {
+            logger.warn("Unable to parse json string: " + jsonString, ex);
+        }
+        return getJsonValues(json, path, 1);
+    }
+
+    private static List<String> getJsonValues(Object json, String[] path, int index) {
+        if (json == null) {
+            return new ArrayList<String>();
+        }
+
+        if (index >= path.length) {
+            return Arrays.asList(String.valueOf(json));
+        }
+
+        if (json instanceof JSONObject) {
+            return getJsonValues(((JSONObject) json).get(path[index]), path, index + 1);
+        }
+
+        if (json instanceof JSONArray) {
+            List<String> values = new ArrayList<String>();
+            Iterator<?> iter = ((JSONArray) json).iterator();
+            while (iter.hasNext()) {
+                values.addAll(getJsonValues(iter.next(), path, index));
+            }
+            return values;
+        }
+
+        logger.warn("Invalid json format: " + json);
+        return new ArrayList<String>();
     }
 
     public static Map<String, String> fetchRow(String keyspace, String columnFamily, String key,
             Set<String> indexColumns) throws Exception {
-        List<ByteBuffer> columnNames = new ArrayList<ByteBuffer>();
-        for (String column : indexColumns) {
-            columnNames.add(ByteBufferUtil.bytes(column));
+        SlicePredicate predicate = new SlicePredicate();
+
+        if (!containsMultiValueColumn(indexColumns)) {
+            // Fetch specific columns
+            List<ByteBuffer> columnNames = new ArrayList<ByteBuffer>();
+            for (String indexColumn : indexColumns) {
+                columnNames.add(ByteBufferUtil.bytes(indexColumn));
+            }
+            predicate.setColumn_names(columnNames);
+        } else if (indexColumns.size() == 1) {
+            // Fetch a slice range
+            SliceRange slice = new SliceRange();
+            String columnPrefix = getColumnPrefix(indexColumns.iterator().next());
+            slice.setStart(ByteBufferUtil.bytes(columnPrefix));
+            slice.setFinish(ByteBufferUtil.bytes(columnPrefix + Character.MAX_VALUE));
+            predicate.setSlice_range(slice);
+        } else {
+            // Fetch all columns
+            SliceRange slice = new SliceRange(ByteBufferUtil.EMPTY_BYTE_BUFFER, ByteBufferUtil.EMPTY_BYTE_BUFFER,
+                    false, 1000);
+            predicate.setSlice_range(slice);
         }
 
-        SlicePredicate predicate = new SlicePredicate();
-        predicate.setColumn_names(columnNames);
-        ColumnParent parent = new ColumnParent(columnFamily);
-        CassandraServer conn = new CassandraServer();
-        conn.set_keyspace(keyspace);
-        List<ColumnOrSuperColumn> columns = conn.get_slice(ByteBufferUtil.bytes(key), parent, predicate,
-                ConsistencyLevel.ONE);
-        Map<String, String> result = new HashMap<String, String>();
+        return getSlice(keyspace, columnFamily, key, predicate);
+    }
 
+    public static Map<String, String> getNewRow(Map<String, String> currentRow, ColumnFamily columnFamily)
+            throws Exception {
+        Map<String, String> mutation = new HashMap<String, String>();
+        for (IColumn column : columnFamily.getSortedColumns()) {
+            String value = column.isMarkedForDelete() ? null : ByteBufferUtil.string(column.value());
+            mutation.put(ByteBufferUtil.string(column.name()), value);
+        }
+
+        Map<String, String> newRow = new HashMap<String, String>(currentRow);
+        newRow.putAll(mutation);
+        return newRow;
+    }
+
+    private static Map<String, String> getSlice(String keyspace, String columnFamily, String key,
+            SlicePredicate predicate) throws Exception {
+        CassandraServer server = new CassandraServer();
+        server.set_keyspace(keyspace);
+        List<ColumnOrSuperColumn> columns = server.get_slice(ByteBufferUtil.bytes(key), new ColumnParent(columnFamily),
+                predicate, ConsistencyLevel.ONE);
+
+        Map<String, String> result = new HashMap<String, String>();
         for (ColumnOrSuperColumn column : columns) {
             result.put(ByteBufferUtil.string(column.column.name), ByteBufferUtil.string(column.column.value));
         }
         return result;
     }
 
-    public static List<LogEntry> getLogEntries(List<IMutation> mutations) throws Exception {
-        List<LogEntry> entries = new ArrayList<LogEntry>();
-
-        for (IMutation mutation : mutations) {
-            String keyspace = mutation.getTable();
-            if (INDEXING_KEYSPACE.equals(keyspace)) {
-                continue;
-            }
-
-            for (ColumnFamily cf : ((RowMutation) mutation).getColumnFamilies()) {
-                entries.add(new LogEntry(keyspace, cf.metadata().cfName, ByteBufferUtil.string(mutation.key()),
-                        Status.PENDING, getRowMutation(cf)));
+    private static boolean containsMultiValueColumn(Collection<String> indexColumns) {
+        for (String indexColumn : indexColumns) {
+            if (isMultiValueColumn(indexColumn)) {
+                return true;
             }
         }
-        return entries;
+        return false;
+    }
+
+    private static boolean isMultiValueColumn(String indexColumn) {
+        return indexColumn.indexOf(Configuration.FIELD_DELIM) >= 0;
+    }
+
+    private static String getColumnPrefix(String indexColumn) {
+        return indexColumn.substring(0, indexColumn.indexOf(Configuration.FIELD_DELIM));
     }
 }
